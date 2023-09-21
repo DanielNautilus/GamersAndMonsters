181f48c3cbe51eefefff711c2c993f07d679178e
DESKTOP-ANTQNA5\kmilo <kmiloslavskiy@gmail.com>
2023-09-21 20:28:25 +0300

    init: set base classes: creatur, randomizer

---
+++ Classes/Randomizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamersAndMonsters.Classes
+{
+    internal class Randomizer
+    {
+        private static readonly Random random = new Random();
+        private static readonly int minDice = 1;
+        private static readonly int maxDice = 6;
+
+        public static uint[] TossDice(uint rollCount)
+        {
+            var result = new uint[rollCount];
+            
+            for (var i = 0;i < rollCount; i++)
+            {
+                result[i] = (uint)random.Next(minDice, maxDice + 1);
+            }
+            return result;
+        }
+        public static uint getRandomValueInDiapason(uint[] valuesDiapason)
+        {
+            return (uint)random.Next((int)valuesDiapason.Min(), (int)valuesDiapason.Max() + 1);
+        }
+    }
+}
